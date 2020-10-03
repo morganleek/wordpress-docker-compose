e11a663f1c0abfdb0c3e5fc88ced49e5595ff403
Morgan Leek <morganleek@gmail.com>
2020-10-03 23:14:38 +0800

    Update database-dump.sh

--- database-dump.sh
+++ database-dump.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec 191e514e30ee mysqldump --user wordpress --password=wordpress wordpress > ./database.sql
+docker exec MARIA_DB_CONTAINER_ID mysqldump --user wordpress --password=wordpress wordpress > ./database.sql
